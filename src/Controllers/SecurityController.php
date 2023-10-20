04514d3b9b3cfa16fe97d4a64ee62c34a5ffbde4
tmeunier <theo.meunier41@gmail.com>
2023-10-20 15:17:00 +0200

    fix: add MVC

---
+++ src/Controllers/SecurityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class SecurityController
+{
+    public function login(): void
+    {
+        echo "Ma page login";
+    }
+
+    public function register(): void
+    {
+        echo "Ma page register";
+    }
+
+    public function logout(): void
+    {
+        echo "Ma page logout";
+    }
+}
