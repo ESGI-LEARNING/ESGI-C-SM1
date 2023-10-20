8e69c21a090f3bba6b2d18d02e01c2f47384cbdb
Philippe DELENTE <philippe.delente@gmail.com>
2023-10-20 15:23:14 +0200

    add View

---
+++ Views/main/contact.view.php
@@ -0,0 +1,3 @@
+<?php include '../components/footer.view.php'; ?>
+    <h2>Contact</h2>
+<?php include '../components/footer.view.php'; ?>
---
+++ Views/main/home.view.php
@@ -0,0 +1,3 @@
+<?php include 'components/header.view.php'; ?>
+<h2>Welcome</h2>
+<?php include 'components/footer.view.php'; ?>
