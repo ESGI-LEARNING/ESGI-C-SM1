949c452857380f7ce431de5b26aa0508ca03ffe1
Philippe DELENTE <philippe.delente@gmail.com>
2023-12-03 12:52:05 +0100

    Fix maincontroller

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -10,4 +10,16 @@ class MainController
     {
         $myView = new View("main/home", "front");
     }
+    public function contact(): void
+    {
+        $myView = new View("main/contact", "front");
+    }
+    public function aboutUs(): void
+    {
+        $myView = new View("main/aboutUs", "front");
+    }
+    public function gallery(): void
+    {
+        $myView = new View("main/gallery", "front");
+    }
 }
