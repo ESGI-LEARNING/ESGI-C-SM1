25a9bb4f678cf09cf882e5ef0925ece8e4df04b9
tmeunier <theo.meunier41@gmail.com>
2023-12-12 11:08:35 +0100

    feat: create session interface

---
+++ core/Session/PHPSession.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Core\Session;
+
+class PHPSession implements SessionInterface
+{
+    public function get(string $key, mixed $default = null): mixed
+    {
+        $this->ensureStarted();
+        return $_SESSION[$key] ?? $default;
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $this->ensureStarted();
+        $_SESSION[$key] = $value;
+    }
+
+    public function delete(string $key): void
+    {
+        $this->ensureStarted();
+        unset($_SESSION[$key]);
+    }
+
+    /**
+     * Vérifie si la session est démarrée
+     */
+    private function ensureStarted(): void
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+}
