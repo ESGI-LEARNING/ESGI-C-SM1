471582f93c0d8de288f4ed0a270f655e4f801f67
Philippe DELENTE <philippe.delente@gmail.com>
2024-01-08 19:54:46 +0100

    add helper function assetLoader

--- core/Helper.php
+++ core/Helper.php
@@ -13,6 +13,24 @@ function url(string $url, ?array $params = []): string
     return config('app.url') . $url . '/' . implode('/', $params);
 }
 
+function assetLoader(): string
+{
+    $manifest = json_decode(file_get_contents(__DIR__.'/../public/build/manifest.json'), true);
+    $css      = $manifest['assets/js/app.css']['file'];
+    $js       = $manifest['assets/js/app.js']['file'];
+
+    if (config('app.env') === 'dev') {
+        return <<<HTML
+            <script type="module" src="http://localhost:5173/assets/js/app.js"></script>
+            <script type="module" src="http://localhost:5173/@vite/client"></script>
+        HTML;
+    }
+
+    return <<<HTML
+            <link rel="stylesheet" href="/build/{$css}">
+            <script src="/build/{$js}"></script>
+        HTML;
+}
 /** Pour le dev **/
 function dd(mixed $var): void
 {
