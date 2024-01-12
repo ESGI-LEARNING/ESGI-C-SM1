9e2b8be48f667e149228a31e4c8badc37a728b5f
Tinou95 <qandrieu@myges.fr>
2024-01-12 18:13:43 +0100

    fix: raccourci(<?= et jointure)

--- views/admin/comments/index.view.php
+++ views/admin/comments/index.view.php
@@ -7,20 +7,20 @@
                 <th>Content</th>
                 <th>Reported</th>
                 <th>Deleted</th> 
-                <th>User Id</th>
+                <th>User</th>
                 <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($comments as $comment) { ?>
                 <tr>
-                    <td><?php echo $comment->getId(); ?></td>
-                    <td><?php echo $comment->getContent(); ?></td>
-                    <td><?php echo $comment->isReported() ? 'Yes' : 'No'; ?></td>
-                    <td><?php echo $comment->getIsDeleted() ? 'Yes' : 'No'; ?></td> 
-                    <td><?php echo $comment->getUserId() ?></td>
+                    <td><?= $comment->getId(); ?></td>
+                    <td><?= $comment->getContent(); ?></td>
+                    <td><?= $comment->isReported() ? 'Yes' : 'No'; ?></td>
+                    <td><?= $comment->getIsDeleted() ? 'Yes' : 'No'; ?></td> 
+                    <td><?= $comment->getUser()->getUsername(); ?></td>
                     <td>
-                        <form method="POST" action="/admin/comments/delete/<?php echo $comment->getId(); ?>" onsubmit="return confirm('Are you sure?')">
+                        <form method="POST" action="/admin/comments/delete/<?= $comment->getId(); ?>" onsubmit="return confirm('Are you sure?')">
                             <button type="submit">Delete</button>
                         </form>
                     </td>
