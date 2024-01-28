5f6115a67aba77903df670a6f542f2711f26bc91
Tinou95 <qandrieu@myges.fr>
2024-01-28 16:14:02 +0100

    feat: chartjs

--- views/admin/dashboard.view.php
+++ views/admin/dashboard.view.php
@@ -1,21 +1,34 @@
-<section class="dashboard-container">
-	<h2>Tableau de bords</h2>
-	<ul class="dashboard-list">
-		<li class="dashboard-card">
-			<h3>Nombre de commentaires</h3>
-			<p><?= $nbComments; ?></p>
-		</li>
-		<li class="dashboard-card">
-			<h3>Nombre de pages</h3>
-			<p>0</p>
-		</li>
-		<li class="dashboard-card">
-			<h3>Nombre d'utilisateurs</h3>
-			<p><?= $nbUsers; ?></p>
-		</li>
-		<li class="dashboard-card">
-			<h3>Nombre d'images</h3>
-			<p><?= $nbImages; ?></p>
-		</li>
-	</ul>
-</section>
+<div class="admin-container">
+    <section class="dashboard-container">
+        <h2>Tableau de bord</h2>
+        <ul class="dashboard-list">
+            <li class="dashboard-card">
+                <h3>Nombre de commentaires</h3>
+                <p><?= $nbComments; ?></p>
+            </li>
+            <li class="dashboard-card">
+                <h3>Nombre de pages</h3>
+                <p>0</p>
+            </li>
+            <li class="dashboard-card">
+                <h3>Nombre d'utilisateurs</h3>
+                <p><?= $nbUsers; ?></p>
+            </li>
+            <li class="dashboard-card">
+                <h3>Nombre d'images</h3>
+                <p><?= $nbImages; ?></p>
+            </li>
+            <li class="dashboard-card">
+                <h3>Statistiques des commentaires</h3>
+                <canvas id="commentDonut"></canvas>
+            </li>
+            <li class="dashboard-card">
+                <h3>Statistiques des utilisateurs</h3>
+                <canvas id="userDonut" ></canvas>
+            </li>
+        </ul>
+    </section>
+</div>
+<script>
+    const [nbUsers, nbComments, nbImages, nbDeletedUsers, nbReportedComments, nbDeletedComments] = [<?= $nbUsers ?>, <?= $nbComments ?>, <?= $nbImages ?>, <?= $nbDeletedUsers ?>, <?= $nbReportedComments ?>, <?= $nbDeletedComments ?>];
+</script>
