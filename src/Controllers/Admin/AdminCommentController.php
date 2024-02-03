293e7f9245ff052ced473719a14a87593d774968
Tinou95 <qandrieu@myges.fr>
2024-02-03 14:16:18 +0100

    feat: pagination_comments

--- src/Controllers/Admin/AdminCommentController.php
+++ src/Controllers/Admin/AdminCommentController.php
@@ -5,17 +5,36 @@ namespace App\Controllers\Admin;
 use App\Models\Comment;
 use App\Mails\CommentMail;
 use Core\Controller\AbstractController;
+use Core\Pagination\Paginator;
 use Core\Views\View;
+use Core\Pagination\HttpRequest;
 
 class AdminCommentController extends AbstractController
 {
     public function index(): View
     {
+        $itemsPerPage = 10;
+        $commentModel = new Comment();
+        $totalItems = $commentModel->count();
     
-        return $this->render('admin/comments/index', 'back', [
-            'comments' => Comment::findAll(),
-        ]);
-    }
+        // Utilisation de la classe HttpRequest pour récupérer le numéro de page
+        $page = HttpRequest::getIntParam('page');
+    
+     // Création d'un objet Paginator pour gérer la pagination
+     $paginator = new Paginator($totalItems, $itemsPerPage, $page, '/admin/comments?page=%d');
+     $offset = $paginator->getOffset(); // Calcul de l'offset à utiliser dans la requête SQL
+
+     // Récupération des commentaires à afficher sur la page actuelle
+     $comments = $commentModel->findWithPagination($itemsPerPage, $offset);
+
+     // Rendu de la vue avec les commentaires et le paginateur
+     return $this->render('admin/comments/index', 'back', [
+         'comments' => $comments, // Liste des commentaires à afficher
+         'paginator' => $paginator, // Objet Paginator pour gérer la pagination dans la vue
+     ]);
+ }
+    
+    
 
     public function delete(int $id): void
     {
