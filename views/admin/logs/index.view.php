2a8c57d2bb682ae13e48b72d2bdc585ab4a20699
tmeunier <theo.meunier41@gmail.com>
2024-02-04 17:28:11 +0100

    add file pagination

---
+++ views/admin/logs/index.view.php
@@ -0,0 +1,34 @@
+<section class="table-view-container">
+    <div class="table-view-header">
+        <h2>Logs</h2>
+    </div>
+    <table>
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Utilisateur</th>
+            <th>Sujet</th>
+            <th>Créer le</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($logs as $log): ?>
+            <tr>
+                <td><?= $log->getId(); ?></td>
+                <td><?= $log->getId(); ?></td>
+                <td><?= $log->getSubject(); ?></td>
+                <td><?= $log->getCreatedAt(); ?></td>
+                <td class="tableau-action">
+
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <div class="pagination">
+        <?= $this->component('pagination', $logs->links()); ?>
+    </div>
+</section>
+
