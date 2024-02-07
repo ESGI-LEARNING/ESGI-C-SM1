4ce67246bd53f82d2c50c1ed2c8e94d546056d50
Tinou95 <qandrieu@myges.fr>
2024-02-07 18:42:58 +0100

    style: article

--- views/main/article.view.php
+++ views/main/article.view.php
@@ -1,4 +1,19 @@
+<?php if ($images): ?>
+    <div class="gallery-container">
+        <?= $this->component('gallery', $config = $images); ?>
+    </div>
+<?php endif; ?>
+
 <div class="article">
+    <?php if ($article->image): ?>
+        <img src="<?= $articleImage ?>" alt="<?= $article->name ?>" class="article-image">
+    <?php endif; ?>
+
+    <h2 class="article-title"><?= $article->name ?></h2>
+    <p class="article-description">Description: <?= $article->description ?></p>
+    <?php if ($article->username): ?>
+        <p class="article-creator">Créateur: <?= $article->username ?></p>
+    <?php endif; ?>
     <?php if ($article->image[0]) { ?>
         <img src="<?= $article->image[0]->image(400, 300); ?>" alt="<?= $article->name; ?>" class="article-image">
     <?php } ?>
