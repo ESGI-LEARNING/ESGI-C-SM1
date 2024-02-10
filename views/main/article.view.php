3b81153f69f88c67b00095cf9cd82e9357cd15e7
Tinou95 <qandrieu@myges.fr>
2024-02-10 13:46:56 +0100

    style: article

--- views/main/article.view.php
+++ views/main/article.view.php
@@ -1,34 +1,21 @@
-<?php if ($images): ?>
-    <div class="gallery-container">
-        <?= $this->component('gallery', $config = $images); ?>
-    </div>
-<?php endif; ?>
-
-<div class="article">
-    <?php if ($article->image): ?>
-        <img src="<?= $articleImage ?>" alt="<?= $article->name ?>" class="article-image">
-    <?php endif; ?>
-
-    <h2 class="article-title"><?= $article->name ?></h2>
-    <p class="article-description">Description: <?= $article->description ?></p>
-    <?php if ($article->username): ?>
-        <p class="article-creator">Créateur: <?= $article->username ?></p>
-    <?php endif; ?>
+<div class="card-photo" >
     <?php if ($article->image[0]) { ?>
-        <img src="<?= $article->image[0]->image(400, 300); ?>" alt="<?= $article->name; ?>" class="article-image">
+        <img src="<?= $article->image[0]->image(400, 300); ?>" alt="<?= $article->name; ?>">
     <?php } ?>
-
-    <h2 class="article-title"><?= $article->name; ?></h2>
-    <p class="article-description">Description: <?= $article->description; ?></p>
-    <?php if ($article->username) { ?>
-        <p class="article-creator">Créateur: <?= $article->username; ?></p>
-    <?php } ?>
-
-    <!-- Formulaire de commentaire -->
-    <form action="/articles/create/<?= $article->id ?>" method="post">
-        <input type="hidden" name="article_id" value="<?= $article->id ?>">
+    <div class="card-photo-info">
+        <h2 class="card-photo-title"><?= $article->name; ?></h2>
+        <p class="article-description">Description: <?= $article->description; ?></p>
+        <?php if ($article->username) { ?>
+            <p class="card-photo-author">Créateur: <?= $article->username; ?></p>
+        <?php } ?>
+    </div>
+</div>
+<form class="form" action="/articles/create/<?= $article->id ?>" method="post">
+    <input type="hidden" name="article_id" value="<?= $article->id ?>">
+    <fieldset>
         <label for="comment">Votre commentaire :</label><br>
         <textarea id="comment" name="comment" rows="4" cols="50"></textarea><br>
         <input type="submit" value="Envoyer">
-    </form>
-</div>
+    </fieldset>
+</form>
+
