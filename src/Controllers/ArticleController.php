2fab076df5422316c75239332592822916411823
Tinou95 <qandrieu@myges.fr>
2024-02-17 14:56:33 +0100

    feat: deleteComment

--- src/Controllers/ArticleController.php
+++ src/Controllers/ArticleController.php
@@ -23,6 +23,8 @@ class ArticleController extends AbstractController
             ->with(['user'])
             ->join('picture_comment', 'picture_comment.comment_id', '=', 'comment.id')
             ->where('picture_comment.picture_id', '=', $article->getId())
+            ->where('comment.is_deleted', '=', false)
+            ->where('comment.is_reported', '=', false)
             ->orderBy('esgi_comment.created_at', 'DESC')
             ->paginate(10, (int)($this->request()->get('page')));
 
@@ -37,7 +39,6 @@ class ArticleController extends AbstractController
     {
         $content = $_POST['comment'];
 
-        // Vérifie si l'utilisateur est authentifié
         if (empty($content)) {
             $this->addFlash('error', 'Le commentaire ne peut pas être vide.');
             $this->redirect("/article/{$pictureId}");
@@ -49,7 +50,6 @@ class ArticleController extends AbstractController
                 'created_at' => date('Y-m-d H:i:s'),
             ];
 
-            // Insertion du commentaire dans la base de données
             $commentModel = new Comment();
             $queryBuilder = $commentModel->query();
             $savedComment = $queryBuilder->save($comment, $commentModel);
@@ -66,50 +66,98 @@ class ArticleController extends AbstractController
         $this->redirect("/article/{$picture->getSlug()}");
     }
 
-    
-
     public function reportComment(int $commentId)
     {
-        // Find the comment by ID
-        $comment = Comment::find($commentId);
+        // Verify CSRF token
+        if ($this->verifyCsrfToken()) {
+            // Find the comment by ID
+            $comment = Comment::find($commentId);
     
-        if ($comment) {
-            // Set is_reported to 1
-            $comment->setIsReported(true);
-            // Save the changes to the database
-            $comment->save();
-    
-            // Fetch all admin users
-            $adminUsers = User::query()
-                ->join('user_role', 'user.id', '=', 'user_role.user_id')
-                ->join('role', 'user_role.role_id', '=', 'role.id')
-                ->where('role.name', '=', 'ROLE_ADMIN')
-                ->get();
-    
-            // Send email to each admin user
-            foreach ($adminUsers as $admin) {
-                $mail = new CommentMail();
-                $mail->sendReportComment($admin->getEmail(), [
-                    'comment_id' => $comment->getId(),
-                    'content'    => $comment->getContent(),
-                ]);
-            }
-    
-                $picture = Picture::query()
-            ->join('picture_comment', 'picture_comment.picture_id', '=', 'picture.id')
-            ->where('picture_comment.comment_id', '=', $commentId)
-            ->get()[0];
-    
-            if ($picture) {
-                $this->addFlash('success', 'Le commentaire a été signalé avec succès.');
-                // Redirect back to the article page using the slug of the associated picture
-                $this->redirect("/article/{$picture->getSlug()}");
-            } else {
-                $this->addFlash('error', 'L\'article associé à ce commentaire n\'a pas été trouvé.');
-            }
-        }
+            if ($comment) {
 
+                $comment->setIsReported(true);
+                $comment->save();
+    
+                // Fetch all admin users
+                $adminUsers = User::query()
+                    ->join('user_role', 'user.id', '=', 'user_role.user_id')
+                    ->join('role', 'user_role.role_id', '=', 'role.id')
+                    ->where('role.name', '=', 'ROLE_ADMIN')
+                    ->get();
+    
+                // Send email to each admin user
+                foreach ($adminUsers as $admin) {
+                    $mail = new CommentMail();
+                    $mail->sendReportComment($admin->getEmail(), [
+                        'comment_id' => $comment->getId(),
+                        'content'    => $comment->getContent(),
+                    ]);
+                }
+    
+                // Fetch the associated picture
+                $picture = Picture::query()
+                    ->join('picture_comment', 'picture_comment.picture_id', '=', 'picture.id')
+                    ->where('picture_comment.comment_id', '=', $commentId)
+                    ->first();
+    
+                if ($picture) {
+                    $this->addFlash('success', 'Le commentaire a été signalé avec succès.');
+                    $this->redirect("/article/{$picture->getSlug()}");
+                } else {
+                    $this->addFlash('error', 'L\'article associé à ce commentaire n\'a pas été trouvé.');
+                }
+            } else {
+                $this->addFlash('error', 'Commentaire non trouvé.');
+            }
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide.');
+        }
+    
+        $this->redirect('/');
     }
     
+    public function deleteComment(int $commentId)
+    {
+
+        if (!Auth::check()) {
+            $this->addFlash('error', 'Vous devez être connecté pour supprimer un commentaire.');
+            $this->redirect("/");
+        }
+    
+        // Verify CSRF token
+        if ($this->verifyCsrfToken()) {
+
+            $comment = Comment::find($commentId);
+    
+            if ($comment) {
+                if ($comment->user_id !== Auth::id()) {
+                    $this->addFlash('error', 'Vous n\'êtes pas autorisé à supprimer ce commentaire.');
+                    $this->redirect("/gallery");
+                }
+    
+                $comment->setIsDeleted(true);
+                $comment->save();
+
+                $picture = Picture::query()
+                    ->join('picture_comment', 'picture_comment.picture_id', '=', 'picture.id')
+                    ->where('picture_comment.comment_id', '=', $commentId)
+                    ->first();
+    
+                if ($picture) {
+                    $this->addFlash('success', 'Le commentaire a été supprimé avec succès.');
+                    $this->redirect("/article/{$picture->getSlug()}");
+                } else {
+                    $this->addFlash('error', 'L\'article associé à ce commentaire n\'a pas été trouvé.');
+                    $this->redirect("/gallery");
+                }
+            } else {
+                $this->addFlash('error', 'Le commentaire que vous essayez de supprimer n\'existe pas.');
+                $this->redirect("/gallery");
+            }
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide.');
+            $this->redirect("/gallery");
+        }
+    }
     
 }
