5b8f6def1209db9fbdf271d328600a91fbc6d520
Tinou95 <quentin95170@hotmail.fr>
2024-02-19 12:00:25 +0100

    feat: envoieEmailTo(admin,reporter,reported)

--- src/Mails/CommentMail.php
+++ src/Mails/CommentMail.php
@@ -6,6 +6,7 @@ use Core\Mailer\Mailer;
 
 class CommentMail
 {
+    //mail pour l'admin
     public function sendReportComment(string $email, array $data): void
     {
         $mail = new Mailer();
@@ -14,6 +15,30 @@ class CommentMail
             'Signalement d\'un commentaire',
             'comment/report-comment',
             'comment/report-comment',
+            $data
+        );
+    }
+
+    public function sendReportCommentToUserReported(string $email, array $data): void
+    {
+        $mail = new Mailer();
+        $mail->send(
+            $email,
+            'Votre commentaire a été signalé',
+            'comment/report-comment-to-user',
+            'comment/report-comment-to-user',
+            $data
+        );
+    }
+
+    public function sendReportCommentToUserReporter(string $email, array $data): void
+    {
+        $mail = new Mailer();
+        $mail->send(
+            $email,
+            'Votre signalement a bien été pris en compte',
+            'comment/report-comment-to-user-reporter',
+            'comment/report-comment-to-user-reporter',
             $data
         );
     }
---
+++ views/mails/comment/report-comment-to-user.html.php
@@ -0,0 +1,15 @@
+<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td>
+            Bonjour, <?= $username; ?>!
+        </td>
+    </tr>
+</table>
+
+<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td>
+            Vous recevez cet e-mail car votre commentaire a été signalé.
+        </td>
+    </tr>
+</table>
