e3ca66858c8af1c21948a7883afa996e38fa4c74
Philippe DELENTE <philippe.delente@gmail.com>
2024-02-20 11:00:24 +0100

    fix

--- views/main/aboutUs.view.php
+++ views/main/aboutUs.view.php
@@ -1,7 +1,5 @@
 <section>
-	<h2>A propos</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam debitis, ducimus, est eveniet, exercitationem
-		iusto
-		labore natus nemo nihil nisi quis voluptas! Et hic illum magni perspiciatis placeat saepe ut.</p>
+	<h2>à propos de nous</h2>
+	<?= $page->content; ?>
 </section>
 
