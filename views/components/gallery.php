559f7fb1a05547f25b10c1db4f49c216576d4947
Tinou95 <quentin95170@hotmail.fr>
2024-02-20 15:19:24 +0100

    fix: afficheimagegalerie

--- views/components/gallery.php
+++ views/components/gallery.php
@@ -1,9 +1,9 @@
 <div class="gallery gallery-container">
-    <?php foreach ($config as $image) { ?>
+    <?php foreach ($config as $image): ?>
         <article>
             <a href="/article/<?= $image->slug; ?>" class="gallery-item">
-	            <img src="<?= $image->image[0]->image(300, 400); ?>" alt="<?= $image->name; ?>">
+                <img src="<?= $image->image; ?>" alt="<?= $image->name; ?>">
             </a>
         </article>
-    <?php } ?>
+    <?php endforeach; ?>
 </div>
