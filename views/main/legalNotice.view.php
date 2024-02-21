3637c7a26f6750b8656ebf9662157aacb2633183
Philippe DELENTE <philippe.delente@gmail.com>
2024-02-21 19:36:52 +0100

    mention-legale

---
+++ views/main/legalNotice.view.php
@@ -0,0 +1,21 @@
+<section>
+    <h2>Mentions légales</h2>
+    <p>
+        Ce site web a été créé dans le cadre d'un projet d'études et n'a aucune vocation commerciale. Il est réalisé par Théo, Quentin, Philippe dans le cadre du cursus 3IW1 à l'ESGI.
+    </p>
+    <p>
+        Contact : contact@theomeunier.fr
+    </p>
+    <p>
+        Tous les contenus présents sur ce site sont utilisés à des fins éducatives et sont la propriété de leurs auteurs respectifs.
+    </p>
+    <p>
+        Pour toute question ou demande de modification, veuillez nous contacter à l'adresse suivante : contact@theomeunier.fr.
+    </p>
+    <p>
+        Hébergement : OVH
+    </p>
+    <p>
+        Dernière mise à jour : 21/02/2023
+    </p>
+</section>
