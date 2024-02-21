d387bbb691ad15cd2f65aea84fced5703091665d
Philippe DELENTE <philippe.delente@gmail.com>
2024-02-21 11:12:39 +0100

    fix sidebar admin

--- views/admin/logs/index.view.php
+++ views/admin/logs/index.view.php
@@ -1,30 +1,34 @@
 <section class="table-view-container">
-    <div class="table-view-header">
-        <h2>Logs</h2>
-    </div>
-    <table>
-        <thead>
-        <tr>
-            <th>Id</th>
-            <th>Utilisateur</th>
-            <th>Sujet</th>
-            <th>Créer le</th>
-        </tr>
-        </thead>
-        <tbody>
+	<div class="table-view-header">
+		<h2>Logs</h2>
+	</div>
+	<table>
+		<thead>
+		<tr>
+			<th>Id</th>
+			<th>Utilisateur</th>
+			<th>Sujet</th>
+			<th>Créer le</th>
+		</tr>
+		</thead>
+		<tbody>
         <?php foreach ($logs as $log) { ?>
-            <tr>
-                <td><?= $log->getId(); ?></td>
-                <td><?= $log->user->getUsername(); ?></td>
-                <td><?= $log->getSubject(); ?></td>
-                <td><?= $log->getCreatedAt(); ?></td>
-            </tr>
+			<tr>
+			<td><?= $log->getId(); ?></td>
+            <?php if ($log->user == null): ?>
+				<td></td>
+            <?php else: ?>
+				<td><?= $log->user->getUsername(); ?></td>
+			<?php endif; ?>
+				<td><?= $log->getSubject(); ?></td>
+				<td><?= $log->getCreatedAt(); ?></td>
+				</tr>
         <?php } ?>
-        </tbody>
-    </table>
+		</tbody>
+	</table>
 
-    <div class="pagination">
+	<div class="pagination">
         <?= $this->component('pagination', $logs->links()); ?>
-    </div>
+	</div>
 </section>
 
