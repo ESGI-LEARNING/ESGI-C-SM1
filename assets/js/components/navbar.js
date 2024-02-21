ff5165ac77ce0d3702dd660f353f405f5221c9b7
Philippe DELENTE <philippe.delente@gmail.com>
2024-02-21 11:02:39 +0100

    fix sidebar admin

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.menu');
-
-	burger.addEventListener('click', function () {
-		if (menu.classList.contains('active')) {
-			menu.classList.remove('active');
-		} else {
-			menu.classList.add('active');
-		}
-	});
+	if(burger && menu) {
+		burger.addEventListener('click', function () {
+			if (menu.classList.contains('active')) {
+				menu.classList.remove('active');
+			} else {
+				menu.classList.add('active');
+			}
+		});
+	}
 });
