f150b30c9ba8d26cfce035fcf149e33397fc2e8f
Tinou95 <quentin95170@hotmail.fr>
2024-02-22 00:31:34 +0100

    fix: carousel

--- assets/js/components/carousel.js
+++ assets/js/components/carousel.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const itemWidth = carouselItems[0].clientWidth;
     let currentIndex = 0;
 
-    // Set width of carousel to fit all items
     carousel.style.width = totalItems * itemWidth + "px";
 
     function goToIndex(index) {
@@ -15,12 +14,25 @@ document.addEventListener("DOMContentLoaded", function() {
         carousel.style.transform = `translateX(${offset}px)`;
     }
 
-    // Previous button click event
+    function toggleNavigation() {
+        const prevBtn = document.getElementById("prevBtn");
+        const nextBtn = document.getElementById("nextBtn");
+        
+        if (totalItems <= 1) {
+            prevBtn.style.display = "none";
+            nextBtn.style.display = "none";
+        } else {
+            prevBtn.style.display = "block";
+            nextBtn.style.display = "block";
+        }
+    }
+
+    toggleNavigation();
+
     document.getElementById("prevBtn").addEventListener("click", function() {
         goToIndex(currentIndex - 1);
     });
 
-    // Next button click event
     document.getElementById("nextBtn").addEventListener("click", function() {
         goToIndex(currentIndex + 1);
     });
